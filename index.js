4b6a9393e023428a31230fb99b940bad3093dfda
MarkusMaal2 <markus.maal@voco.ee>
2023-01-06 12:30:54 +0200

    Fixed author name not showing, when they haven't written any articles.

--- index.js
+++ index.js
@@ -60,13 +60,17 @@ app.get('/article/:slug', (req, res) => {
 })
 
 app.get('/author/:author_id', (req, res) => {
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${req.params.author_id};`
+    let get_author = `SELECT name FROM author WHERE id = ${req.params.author_id}`
+    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${req.params.author_id};`
     let author = ''
-    con.query(query, (err, result) => {
+    con.query(get_author, (err, result) => {
         if (err) throw err
         if (result.length > 0) {
-            author = result[0].author
+            author = result[0].name
         }
+    })
+    con.query(query, (err, result) => {
+        if (err) throw err
         res.render('author', {
             articles: result,
             author: author
