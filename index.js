f1886a04dafb3c9b807a907c168e07b55ad4d51f
MarkusMaal2 <markus.maal@voco.ee>
2023-01-06 12:21:01 +0200

    Displayed author article list

--- index.js
+++ index.js
@@ -50,11 +50,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.id as 'id', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author' FROM article INNER JOIN author ON article.author_id = author.id WHERE slug = '${req.params.slug}'`
+    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE slug = '${req.params.slug}'`
     con.query(query, (err, result) => {
         if (err) throw err
         res.render('article', {
             article: result
+        })
+    })
+})
+
+app.get('/author/:author_id', (req, res) => {
+    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${req.params.author_id};`
+    let author = ''
+    con.query(query, (err, result) => {
+        if (err) throw err
+        if (result.length > 0) {
+            author = result[0].author
+        }
+        res.render('author', {
+            articles: result,
+            author: author
         })
     })
 })
